1b61c4ada61ec7b561e3a3989a9f5cb554e76120
boris.stojanovic <boris.stojanovic@msg-global.com>
2021-10-20 10:42:55 +0200

    Testing with verify mockito method

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -21,7 +22,38 @@ class SpecialitySDJpaServiceTest {
     @Test
     void testDeleteByID() {
         service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, times(2)).deleteById(1L);
     }
+
+    @Test
+    void testDeleteByIDAtLeast() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+    }
+
+    @Test
+    void testDeleteByIDAtMost() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void testDeleteByIDNever() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+
+        //verify that its never called with value 5L
+        verify(specialtyRepository, never()).deleteById(5L);
+    }
+
 
     @Test
     void testDelete() {
